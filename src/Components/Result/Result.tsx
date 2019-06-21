59d0737ed046791de3c1312dad94fa9ad4776128
Nat <usolnatalia@gmail.com>
2019-06-21 16:05:41 +1200

    Changed the styling

--- src/Components/Result/Result.tsx
+++ src/Components/Result/Result.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Loader from 'react-loader-spinner'
 
+
 interface IProps{
   result:string
   filelength:any
@@ -11,11 +12,15 @@ export default class Result extends React.Component<IProps,{}> {
   public render() {
     return (
       <div className="result">
+        
         {
           this.props.result === "" && this.props.filelength>0 ?
-            <Loader type="TailSpin" color="#00BFFF" height={80} width={80} /> :
-            <p>{this.props.result}</p>
+            <Loader type="Puff" color="#85144b" height={80} width={80} /> :
+            <div >
+              <p> {this.props.result} </p>
+            </div>
         }
+        
       </div>
     )
   }
